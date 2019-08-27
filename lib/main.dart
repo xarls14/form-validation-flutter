09491bc0c98bddc8f520cf1e17079b3d5719e85d
Henriquez <ch250338@ncr.com>
2019-08-27 11:03:05 -0400

    Form validation flutter - agregando validaciones

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/provider.dart';
+import 'package:formvalidation/src/pages/home_page.dart';
+import 'package:formvalidation/src/pages/login_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Provider(
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        initialRoute: 'login',
+        routes: {
+          'login' : ( BuildContext context ) => LoginPage(),
+          'home' : ( BuildContext context ) => HomePage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          
+        ],
+      ),
+    );
+  }
+
+ 
+}
